e2cab85f4dfbf3ceec3b0b9ce80d8a108da12b05
Cristian Saavedra Toledo <cristian.saavedra.toledo@gmail.com>
2021-01-23 09:10:32 -0300

    creacion de repositorio

---
+++ app/src/main/java/cl/darkdragonzerox/magicsupport/data/Repository.kt
@@ -0,0 +1,27 @@
+package cl.darkdragonzerox.magicsupport.data
+
+import androidx.lifecycle.LiveData
+
+class Repository {
+    private val magicDatabase =MagicApplication.magicDatabase!!
+    val magicList= magicDatabase.MagicDao().getMagicCards()
+    suspend fun getCardsFromApi(){
+        val response =RetrofitClient.retrofitCliente().getMagicCards()
+        when(response.isSuccessful){
+            true ->{
+                response.body()?.let {
+                    magicDatabase.MagicDao().LoadAllCards(it)
+                }
+
+
+            }
+            false -> {
+
+            }
+        }
+    }
+    fun getCard(idCode :String): LiveData<MagicCards>{
+        return  magicDatabase.MagicDao().getCardDetail(idCode)
+    }
+
+}
