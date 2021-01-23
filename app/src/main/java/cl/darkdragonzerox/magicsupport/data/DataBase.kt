0a5f8230294b6e56d68cbcc8d22fed0c0b39c601
Cristian Saavedra Toledo <cristian.saavedra.toledo@gmail.com>
2021-01-22 21:00:14 -0300

    DB implementada

--- app/src/main/java/cl/darkdragonzerox/magicsupport/data/DataBase.kt
+++ app/src/main/java/cl/darkdragonzerox/magicsupport/data/DataBase.kt
@@ -1,10 +1,11 @@
 package cl.darkdragonzerox.magicsupport.data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
-interface MagigDao{
+interface MagicDao{
     @Insert(onConflict= OnConflictStrategy.REPLACE)
     suspend fun LoadAllCards(cardList:List<MagicCards>)
 
@@ -16,5 +17,19 @@ interface MagigDao{
 
 }
 
-@Database(entities = [MagicCards::class], version = 1)
+@Database(entities = [MagicCards::class],version = 1)
 @TypeConverters(Converters::class)
+abstract class MagicDatabase : RoomDatabase(){
+    abstract fun MagicDao():MagicDao
+}
+class MagicApplication : Application(){
+    companion object{
+        var magicDatabase: MagicDatabase? =null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        magicDatabase=Room
+            .databaseBuilder(this,MagicDatabase::class.java,"db_MagicCards").build()
+    }
+}
